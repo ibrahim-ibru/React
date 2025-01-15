65b46186a408cc98de131fd9b5610f4757535f05
ibrahim-ibru <mohammedibrahim2k02@gmail.com>
2025-01-15 11:10:54 +0530

    updtaed

--- functionalcomponent/src/App.jsx
+++ functionalcomponent/src/App.jsx
@@ -3,15 +3,26 @@ import Head from "./components/header"
 
 function App(){
   const data=" HOME"
-  let [name,setName]=useState(0)
-  const handleClick=()=>{
-    setName(name+=1)
+  let [value,setValue]=useState({name:"",mark:"",cls:""})
+  
+  const handleChange=(e)=>{
+    setValue((pre)=>({...pre,[e.target.name]:e.target.value}))
+  }
+
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    const {name,mark,cls}=value
+    alert(`${name} ${mark} ${cls}`)
+    setValue({name:"",mark:"",cls:""})
   }
   return(
     <>
-    <button onClick={handleClick}> Click</button>
-    <h1>Hello Team {name}</h1>
-    <Head data={data}/>
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="name" name="name" value={value.name} onChange={handleChange} />
+      <input type="text" placeholder="mark" name="mark" value={value.mark} onChange={handleChange} />
+      <input type="text" placeholder="cls" name="cls" value={value.cls} onChange={handleChange} />
+      <input type="submit" value={"Add Data"}/>
+    </form>
     </>
   )
 }
