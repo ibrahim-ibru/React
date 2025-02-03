b863be2f0b1d3aaf6a10b0f6873fdddb1418d59b
ibrahim-ibru <mohammedibrahim2k02@gmail.com>
2025-02-03 09:43:07 +0530

    updtaed

---
+++ material_ui/src/App.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+
+
+export default function ButtonUsage() {
+  return (
+    <>
+    <Button variant="contained">Hello world</Button>
+    </>
+  )
+}
