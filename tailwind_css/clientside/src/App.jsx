4c25bcb7c9041a573826d7d13d73a2205bbed41b
ibrahim-ibru <mohammedibrahim2k02@gmail.com>
2025-02-04 10:16:14 +0530

    updated

---
+++ tailwind_css/clientside/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>
+      <img className='xs:float-none sm:float-right md:float-left ' src="../public/vite.svg" alt="" />
+      <p >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam ipsum consectetur velit corporis veniam, hic eos officiis soluta neque quis nostrum sapiente tenetur odio odit ad consequuntur sit iusto quisquam?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, possimus ad itaque beatae nisi aspernatur ipsam voluptatem, non vel dicta laborum? Nulla, provident expedita accusamus repudiandae eos rerum iusto beatae.</p>
+    </div>
+    </>
+  )
+}
+
+export default App
